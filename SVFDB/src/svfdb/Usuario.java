94bba62ecba5a8950609aaf8603baf20db678f38
ARTEAGA1811 <bryan.arteaga@epn.edu.ec>
2021-02-14 12:25:35 -0500

    jsjs

--- SVFDB/src/svfdb/Usuario.java
+++ SVFDB/src/svfdb/Usuario.java
@@ -3,20 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package svfdb;
 
 /**
  *
  * @author David Arteaga / Mittsaker / Art3-
+ * 14 feb. 2021 12:24:20 p. m.
  */
-public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        
-    }
+public class Usuario {
+    String login;
+    String password;
     
+    //Metodos
+           
 }
